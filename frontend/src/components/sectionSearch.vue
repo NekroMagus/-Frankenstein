48976b92443f1de532e65e4f3414b5fe00b704cc
Alexey-aka-sultan <alex_240981@mail.ru>
2019-10-18 16:44:58 +0600

    create search vue

---
+++ frontend/src/components/sectionSearch.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <input type="search" size="50" name="" id="">
+      <input type="submit" value="search">
+      <div class="search-result">
+          
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>

23ed80f99edb3d48378465179674a0320296568c
NekroMagus <NekroMagus@mail.ru>
2019-10-18 18:52:29 +0300

    fixed get method

--- backend/index.js
+++ backend/index.js
@@ -4,15 +4,18 @@ const fs = require('fs');
 const allUsers = require(__dirname + '/public/json/user.json');
 
 app.get('/users', async (req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain', 'Access-Control-Allow-Origin': '*'});
-    if (req.query.userId === undefined || req.query.userId === null) {
+    let userId = req.query.userId;
+    if (Number.isInteger(+userId)) {
+        let users = allUsers.filter(item => +item.userId === +req.query.userId);
+        res.writeHead(200, {'Content-Type': 'text/plain', 'Access-Control-Allow-Origin': '*'});
+        res.end(JSON.stringify(users));
+    } else {
+        res.writeHead(200, {'Content-Type': 'text/plain', 'Access-Control-Allow-Origin': '*'});
         res.end(JSON.stringify(allUsers));
     }
-    let user = allUsers.filter(item => +item.userId === +req.query.userId);
-    res.end(JSON.stringify(user));
 });
 
 const server = app.listen(3000, () => {
     let port = server.address().port;
-    console.log("http://localhost:" + port + "/user");
+    console.log("http://localhost:" + port + "/users");
 });

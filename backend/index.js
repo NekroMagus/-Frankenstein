e4342c75731c6352cb0925efc2cd389a8fc0467f
NekroMagus <NekroMagus@mail.ru>
2019-10-18 17:24:20 +0300

    add directory

--- backend/index.js
+++ backend/index.js
@@ -4,19 +4,19 @@ const fs = require('fs');
 const users = require(__dirname + '/public/json/user.json');
 
 app.get('/json', (req, res) => {
-    fs.readFile(__dirname +'/public/json'+ "/user.json", "utf-8", (err, data) => {
-        res.writeHead(200, {'Content-Type': 'text/json', 'Access-Control-Allow-Origin' : '*'});
+    fs.readFile(__dirname + '/public/json' + "/user.json", "utf-8", (err, data) => {
+        res.writeHead(200, {'Content-Type': 'text/json', 'Access-Control-Allow-Origin': '*'});
         res.end(data);
     });
 });
 
 app.get('/json/getuser', (req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain', 'Access-Control-Allow-Origin' : '*'});
-   let data = null;
+    res.writeHead(200, {'Content-Type': 'text/plain', 'Access-Control-Allow-Origin': '*'});
+    let data = null;
     try {
         data = JSON.parse(users);
-    }catch(e) {
-        data =users;
+    } catch (e) {
+        data = users;
     }
     let userId = users.filter(item => +item.userId === +req.param.userId);
     res.end(JSON.stringify(userId));
@@ -25,6 +25,5 @@ app.get('/json/getuser', (req, res) => {
 const server = app.listen(3000, () => {
     let host = server.address().address;
     let port = server.address().port;
-    console.log("http://localhost:" + port + "/json");
+    console.log("http://localhost:" + port + "/json/getuser");
 });
-

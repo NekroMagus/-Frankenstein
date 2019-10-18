16b6cd600d0b6c1d363b47a8806edff19aecd349
NekroMagus <NekroMagus@mail.ru>
2019-10-18 13:44:44 +0300

    add get method

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,15 @@
 const express = require('express');
+const app = express();
+const fs = require('fs');
+
+app.get('/json', (req, res) => {
+    fs.readFile(__dirname + "/package.json", "utf8", (err, data) => {
+        res.end(data);
+    });
+});
+
+const server = app.listen(3000, () => {
+    let host = server.address().address;
+    let port = server.address().port;
+    console.log("http://localhost:" + port);
+});

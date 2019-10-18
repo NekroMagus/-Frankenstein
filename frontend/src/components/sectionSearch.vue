401f3dc95463c49ed213fb252a0170e235bb0260
Alexey-aka-sultan <alex_240981@mail.ru>
2019-10-19 00:20:29 +0600

    fx

--- frontend/src/components/sectionSearch.vue
+++ frontend/src/components/sectionSearch.vue
@@ -1,18 +1,31 @@
 <template>
   <div>
     <hr />
+    <input v-bind="searchedUserId" type="text" placeholder="userid" size="5" />
+    <input v-bind="currentTitle" type="text" placeholder="title" size="50" />
+    <input @click="postComment" type="submit" value="post comment" />
+    <br />
+    <textarea v-bind="currentBody" type="text" placeholder="body" rows="7"></textarea>
+
+    <hr />
     <input type="submit" @click="getAllResults" value="search all users" />
     <hr />
-      <input
-        type="search"
-        name="userId"
-        v-model="searchedUserId"
-        size="10"
-        placeholder="enter user id"
-      />
-      <input type="submit" @click="getResultsById" value="search by id" />
+
+    <input
+      type="search"
+      name="userId"
+      v-model="searchedUserId"
+      size="10"
+      placeholder="enter user id"
+    />
+    <input type="submit" @click="getResultsById" value="search by id" />
+
     <div class="search-result">
-      <div class="search-result__item rounded" v-for="(item, index) in searchedResult" v-bind:key="index">
+      <div
+        class="search-result__item rounded"
+        v-for="(item, index) in searchedResult"
+        v-bind:key="index"
+      >
         <span class="search-result__userId">Id: {{ item.id }}</span>
         <span class="search-result__title">title: {{ item.title }}</span>
         <p class="search-result__body">{{ item.body }}</p>
@@ -27,7 +40,9 @@ export default {
   data() {
     return {
       searchedResult: [],
-      searchedUserId: null
+      searchedUserId: null,
+      currentTitle: '',
+      currentBody: ''
     };
   },
   methods: {
@@ -37,10 +52,20 @@ export default {
       });
     },
     getResultsById: function() {
-      axios.get("http://localhost:3000/users", {params: {userId: this.searchedUserId }}).then(response => {
-        this.searchedResult = response.data;
-        console.log(this.searchedResult);
-      });
+      axios
+        .get("http://localhost:3000/users", {
+          params: { userId: this.searchedUserId }
+        })
+        .then(response => {
+          this.searchedResult = response.data;
+        });
+    },
+    postComment: function() {
+      axios
+        .post("http://localhost:3000/users", { userId: 0, id: b, title: c, body: d })
+        .then(response => {
+          console.log(response);
+        });
     }
   }
 };
@@ -51,7 +76,7 @@ export default {
   border: 1px solid #000;
   margin: 5px 0;
   cursor: pointer;
-  transition: transform .2s;
+  transition: transform 0.2s;
   background-color: #fff;
 }
 .search-result__item:hover {
@@ -72,5 +97,10 @@ export default {
   text-align: right;
   padding-right: 9px;
   font-style: italic;
+}
+
+textarea {
+  width: 100%;
+  resize: none;
 }
 </style>

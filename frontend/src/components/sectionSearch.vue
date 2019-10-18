9b6d5f0f5a507aa70d4510ecb3d304c469f6c7a7
Alexey-aka-sultan <alex_240981@mail.ru>
2019-10-18 19:37:12 +0600

    search vue

--- frontend/src/components/sectionSearch.vue
+++ frontend/src/components/sectionSearch.vue
@@ -1,8 +1,24 @@
 <template>
   <div>
-    <input type="search" size="50" name id />
-    <input type="submit" @click="getSerchResult" value="search" />
-    <div class="search-result"></div>
+    <hr />
+    <input type="submit" @click="getAllResults" value="search all users" />
+    <hr />
+    <input
+      type="search"
+      name="userId"
+      v-model="searchedUserId"
+      size="10"
+      placeholder="enter user id"
+    />
+    <input type="submit" @click="getResultsById" value="search by id" />
+    <div class="search-result">
+      <div class="search-result__item rounded" v-for="(item, index) in searchedResult" v-bind:key="index">
+        <span class="search-result__userId">Id: {{ item.id }}</span>
+        <span class="search-result__title">title: {{ item.title }}</span>
+        <p class="search-result__body">{{ item.body }}</p>
+        <p class="search-result__user-id">userId: {{item.userId}}</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,19 +26,56 @@
 export default {
   data() {
     return {
-      info: null
+      searchedResult: [],
+      searchedUserId: null
     };
   },
   methods: {
-    getSerchResult: function() {
+    getAllResults: function() {
       axios.get("http://localhost:3000/json").then(response => {
-        this.info = response.data;
-        console.log(this.info);
+        this.searchedResult = response.data;
       });
+    },
+    getResultsById: function() {
+      axios.get("http://localhost:3000/json/getuser").then(response => {
+        this.info = response.data;
+        console.log(this.searchedResult);
+      });
+    }
+  },
+  watch: {
+    searchedUserId: function(value) {
+      console.log(value);
     }
   }
 };
 </script>
 
 <style>
+.search-result__item {
+  border: 1px solid #000;
+  margin: 5px 0;
+  cursor: pointer;
+  transition: transform .2s;
+  background-color: #fff;
+}
+.search-result__item:hover {
+  transform: scale(1.1);
+}
+
+.search-result__userId {
+  background-color: cornflowerblue;
+  color: white;
+  padding: 2px 4px;
+}
+.search-result__title {
+  padding-left: 15px;
+  font-weight: 600;
+}
+
+.search-result__user-id {
+  text-align: right;
+  padding-right: 9px;
+  font-style: italic;
+}
 </style>

dd9086b3185585204bb504cf9c20269f4f320248
Alexey-aka-sultan <alex_240981@mail.ru>
2019-11-02 22:42:41 +0600

    fx

--- frontend/src/components/downloadFilePage.vue
+++ frontend/src/components/downloadFilePage.vue
@@ -1,16 +1,21 @@
 <template>
   <div>
-      <h2>download file</h2>
-      
+    <h2>download file</h2>
+    <button @click="downloadFile">download file</button>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  methods: {
+    downloadFile() {
+      axios.get("http://localhost:3000/downloadFiles").then(response => {
+        console.log(response);
+      });
+    }
+  }
+};
 </script>
 
 <style>
-
 </style>
